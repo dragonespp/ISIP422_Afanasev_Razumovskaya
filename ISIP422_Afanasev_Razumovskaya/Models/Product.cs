b214f4e53301e864283306599371d5499bfcd9b3
227061 <227061@KB37-303-C8>
2025-09-22 13:40:28 +0300

    Созданы папки и файлы для проекта

---
+++ ISIP422_Afanasev_Razumovskaya/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP422_Afanasev_Razumovskaya.Models
+{
+    internal class Product
+    {
+    }
+}
