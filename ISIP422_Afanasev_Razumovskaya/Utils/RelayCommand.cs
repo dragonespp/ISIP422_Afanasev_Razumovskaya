92dd7de0bf7ae88d81b102ca5ad228a9aa5a50e9
227061 <227061@KB37-303-C8>
2025-09-22 13:50:14 +0300

    Добавлен RelayCommand для реализации команд MVVM

--- ISIP422_Afanasev_Razumovskaya/Utils/RelayCommand.cs
+++ ISIP422_Afanasev_Razumovskaya/Utils/RelayCommand.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ISIP422_Afanasev_Razumovskaya.Utils
 {
-    internal class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
     }
 }
