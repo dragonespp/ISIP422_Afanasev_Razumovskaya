531c73d2c076f6121edca561c9267e7ac20b68e3
227061 <227061@KB37-303-C8>
2025-09-22 13:49:36 +0300

    Добавлена модель истории продаж

--- ISIP422_Afanasev_Razumovskaya/Models/SalesHistory.cs
+++ ISIP422_Afanasev_Razumovskaya/Models/SalesHistory.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace ISIP422_Afanasev_Razumovskaya.Models
 {
-    internal class SalesHistory
+    public class SalesHistory
     {
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public int QuantitySold { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Category Category { get; set; }
+
+        public SalesHistory(Product product, int quantity)
+        {
+            ProductCode = product.Code;
+            ProductName = product.Name;
+            QuantitySold = quantity;
+            UnitPrice = product.Price;
+            TotalAmount = quantity * product.Price;
+            SaleDate = DateTime.Now;
+            Category = product.Category;
+        }
     }
 }
